4b93518cf9d8b8e2573849aab4813b3c82f13e2a
nazarb <nazar.bulawka@gmail.com>
2021-08-29 14:53:52 +0200

    Update Hopkins_Skellam1954_2.R
    Clean version

--- ClarkEvans-master/R/Hopkins_Skellam1954_2.R
+++ ClarkEvans-master/R/Hopkins_Skellam1954_2.R
@@ -9,6 +9,8 @@ tool_exec <- function(in_params, out_params)
   input_feature = NULL 
   output_feature1 = NULL 
   
+    # Install packages on the go
+  
   if (!requireNamespace("sp", quietly = T))
     install.packages("sp")
   if (!requireNamespace("spatstat",quietly = T))
@@ -20,60 +22,43 @@ tool_exec <- function(in_params, out_params)
   if (!requireNamespace("arcgisbinding",quietly = T))
     install.packages("arcgisbinding")
   
+  ## Initialize libraries
+  
   require(sp)
   require(spatstat) 
   require(maptools)
   require(spdep)
   require(arcgisbinding)
-
-  #print("libraries ok")
+  arc.check_product()
+  
+  print("Libraries: OK")
   
   ##Declaring the inputs and Output
   input_feature = in_params[[1]]
-  #input_boundary = in_params[[2]]
-  #Method = in_params[[2]]
-  #MC = in_params[[3]]
   output_feature1 = out_params[[1]]
-  #output_feature2 = out_params[[2]]
-  #print("input and output params ok")
+  print("The input and output params: OK")
   
-  # na razie nie ogarn¹³em jak te dane dobrze wydostaæ R. Akapit ##Declaring the inputs and Output jest domyœlny
-  
-  #open the data
-  #Data1 <- readShapeSpatial("D:/GIS/Margiana/rethemurghabdelta/WektorAnalizy/MurgabYazI_1selection.shp")
-  
-  #Data1 <- readShapeSpatial(input_feature)
-  #Data2 <- as.ppp(Data1)
   Data1 = arc.open(input_feature)
-  #print("Data 1 ok")
+  #print("Data: Step 1: OK")
   Data1a = arc.select(Data1)
-  #print("Data 1a ok")
+  #print("Data: Step 1a: OK")
   Data2a = arc.data2sp(Data1a)
-  #print("Data 2a ok")
+  #print("Data: Step 2a: OK")
   Data2 = as.ppp(Data2a)
-  #print("Data 2 :) ok")
-  #Boxing <- convexhull(Data2)
-  #print("Boxing ok")
-  # Wyswietla, ze cos robi
-  #arc.progress_label("Loading Dataset")
-  # czas na test Clark and Evans 1954
-  #test <- clarkevans.test(Data2, correction="none", clipregion=Boxing, alternative=c(Method), nsim=MC)
+  #print("Data: Step 1: OK")
+  
+  arc.progress_label("Loading Dataset")
+  print("Running interations of Hopkins and Skellam 1954 test")
+  ## Performing Hopkins and Skellam 1954 test
   for(i in 1:999) {
     out[i] <- hopskel(Data2)
     print(out[i])
   }
-  #print("test ok")
-  #test <- clarkevans.test(Data2, correction="none", clipregion=NULL, alternative=c(Regular"), nsim=MC)test
-  #test2 <- hopskel.test(y1, alternative=c("clustered"), method=c("MonteCarlo"), nsim=999)
-  #test2
-  #print(out_params)
   out_params <- mean(out)
   out2 <- sd(out)
-  print("result")
-  print(out_params)
-  print("standart deviation")
+  #print("Running interations of Hopkins and Skellam 1954 test")
+  #print(out_params)
+  print("Standard deviation of 999 interations of Hopkins and Skellam 1954 test:")
   print(out2)
-  print("go")
-  #print(output_feature1)
   return(out_params)
 }
