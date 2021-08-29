e8e65b02d2918fe7af39e071e0b3e487363ec920
nazarb <nazar.bulawka@gmail.com>
2021-08-29 14:38:43 +0200

    Update Arc_ClarkEvans_test2.R

--- ClarkEvans-master/R/Arc_ClarkEvans_test2.R
+++ ClarkEvans-master/R/Arc_ClarkEvans_test2.R
@@ -3,7 +3,7 @@ tool_exec <- function(in_params, out_params)
   Data1 = NULL
   Data2 = NULL
 
-  #rm(list=ls())
+  # Install packages on te go
   
   if (!requireNamespace("sp", quietly = T))
     install.packages("sp")
@@ -15,47 +15,43 @@ tool_exec <- function(in_params, out_params)
     install.packages("spdep")
   if (!requireNamespace("arcgisbinding",quietly = T))
     install.packages("arcgisbinding")
-
+  
+## Initialize libraries
+  
   require(sp)
   require(spatstat) 
   require(maptools)
   require(spdep)
   require(arcgisbinding)
-  print("libraries ok")
+  arc.check_product()
+  
+  print("Libraries: OK")
   
   ##Declaring the inputs and Output
   input_feature = in_params[[1]]
-  #input_boundary = in_params[[2]]
   Method = in_params[[2]]
   MC = in_params[[3]]
   correct = in_params[[4]]
   output_feature1 = out_params[[1]]
-  #output_feature2 = out_params[[2]]
-  print("input and output params ok")
+  print("The input and output params: OK")
 
-  # na razie nie ogarn¹³em jak te dane dobrze wydostaæ R. Akapit ##Declaring the inputs and Output jest domyœlny
 
-  #open the data
-  #Data1 <- readShapeSpatial("D:/GIS/Margiana/rethemurghabdelta/WektorAnalizy/MurgabYazI_1selection.shp")
+  ## Open the data and convert to appropreate format
 
-  #Data1 <- readShapeSpatial(input_feature)
-  #Data2 <- as.ppp(Data1)
   Data1 = arc.open(input_feature)
-  print("Data 1 ok")
+  print("Data: Step 1: OK")
   Data1a = arc.select(Data1)
-  print("Data 1a ok")
+  print("Data: Step 1a: OK")
   Data2a = arc.data2sp(Data1a)
-  print("Data 2a ok")
+  print("Data: Step 2a: OK")
   Data2 = as.ppp(Data2a)
-  print("Data 2 :) ok")
+  print("Data: Step 2: OK")
   Boxing <- convexhull(Data2)
-  print("Boxing ok")
-  # Wyswietla, ze cos robi
-  #arc.progress_label("Loading Dataset")
-  # czas na test Clark and Evans 1954
-  #test <- clarkevans.test(Data2, correction="none", clipregion=Boxing, alternative=c(Method), nsim=MC)
-  #out_params <- clarkevans(Data2, correction=c("all"), clipregion=Boxing)
-  print("test ok")
+  print("Convexhull: OK")
+  
+  arc.progress_label("Loading Dataset")
+  
+  ## Performing Clark and Evans 1954 test
   out_params <- clarkevans.test(Data2, correction=correct, clipregion=Boxing, alternative=c(Method), nsim=MC)
   print(out_params)
   return(out_params)
