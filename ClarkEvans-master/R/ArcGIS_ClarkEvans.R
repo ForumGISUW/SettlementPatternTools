9ecf31fe32174ce3ef27f039c214c56e37e9083f
nazarb <nazar.bulawka@gmail.com>
2021-08-29 14:58:55 +0200

    Update and rename Arc_ClarkEvans2.R to ArcGIS_ClarkEvans.R

--- ClarkEvans-master/R/ArcGIS_ClarkEvans.R
+++ ClarkEvans-master/R/ArcGIS_ClarkEvans.R
@@ -23,33 +23,33 @@ tool_exec <- function(in_params, out_params)
   require(spdep)
   require(arcgisbinding)
   arc.check_product()
-  print("Libraries: OK")
+  #print("Libraries: OK")
   
   ##Declaring the inputs and Output
   input_feature = in_params[[1]]
   output_feature1 = out_params[[1]]
-  print("The input and output params: OK")
+  #print("The input and output params: OK")
 
 
   ## Open the data
 
   Data1 = arc.open(input_feature)
-  print("Data: Step 1: OK")
+  #print("Data: Step 1: OK")
   Data1a = arc.select(Data1)
-  print("Data: Step 1a: OK")
+  #print("Data: Step 1a: OK")
   Data2a = arc.data2sp(Data1a)
-  print("Data: Step 2a: OK")
+  #print("Data: Step 2a: OK")
   Data2 = as.ppp(Data2a)
-  print("Data: Step 2: OK")
+  #print("Data: Step 2: OK")
   Boxing <- convexhull(Data2)
-  print("Convexhull: OK")
+  #print("Convexhull: OK")
 
   arc.progress_label("Loading Dataset")
   
   ## Performing Clark and Evans 1954 test
 
   out_params <- clarkevans(Data2, correction=c("all"), clipregion=Boxing)
-  print("Procedure: OK")
+  #print("Procedure: OK")
 
   print(out_params)
   return(out_params)
